82b47cf4ac1438cca6fe4df959aa0f443fd8fb28
fabricio <ffcintrass@gmail.com>
2023-03-28 17:16:08 -0300

    sessao sobre concluido pc

--- otica/src/componentes/SessaoSobre/index.js
+++ otica/src/componentes/SessaoSobre/index.js
@@ -4,17 +4,17 @@ import React from 'react';
 const SessaoSobre=()=>{
     return(
         <div className='sessao-sobre'>
-            <div>
+            <div className='sessao-sobre-agrupada'>
                 <h2>QUEM SOMOS NOS?</h2>
                 <p>
                     Fundada em 2021, em Nova Iguaçu - Rio de Janeiro, a Ótica vida iniciou com o objetivo de proporcionar ao cliente bom atendimento, qualidade e preço baixo.
                 </p>
 
-                <div>
-                    <div>
+                <div className='grupo-imagem-texto'>
+                    <div className='box-img'>
                         <img src="assets/loja.png"alt="imagem"/>
                     </div>
-                    <div>
+                    <div className='box-texto'>
                        <h3>
                         NOSSAS FILIAIS
                        </h3>
@@ -22,16 +22,18 @@ const SessaoSobre=()=>{
                         Hoje temos mais de 20 filiais pelo Brasil e na América
                        </p>
                     </div>
-                    <div>
-                        <img src="assets/atendimento.png" alt="imagem"/>
-                    </div>
-                    <div>
+                    
+                    <div className='box-texto'>
                         <h3>
-                            ATENDIMENTO <br/> FLEXIVEL
+                            ATENDIMENTO FLEXIVEL
                         </h3>
+                        
                         <p>
                             Nossa equipe possui treinamento para te atender
                         </p>
+                    </div>
+                    <div className='box-img'>
+                        <img src="assets/atendimento.png" alt="imagem"/>
                     </div>
                 </div>
             </div>
